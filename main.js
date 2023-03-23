098faa960ad45e79ab82235344ca96ce21f3a279
Adriano <adrianoeo@outlook.com>
2023-03-23 18:22:15 -0300

    Adiciona implementação de cadeia de Markov inicial

---
+++ main.js
@@ -0,0 +1,50 @@
+let names;
+let order = 2;
+let ngrams = {};
+let beginnings = [];
+let button;
+
+function preload() {
+    names = loadStrings('names.txt');
+    console.log(names);
+  }
+  
+  function setup() {
+    noCanvas();
+    for (let j = 0; j < names.length; j++) {
+      let txt = names[j];
+      for (let i = 0; i <= txt.length - order; i++) {
+        let gram = txt.substring(i, i + order);
+        if (i == 0) {
+          beginnings.push(gram);
+        }
+  
+        if (!ngrams[gram]) {
+          ngrams[gram] = [];
+        }
+        ngrams[gram].push(txt.charAt(i + order));
+      }
+    }
+    button = createButton("generate");
+    button.mousePressed(markovIt);
+    console.log(ngrams);
+  }
+  
+  function markovIt() {
+  
+    let currentGram = random(beginnings);
+    let result = currentGram;
+  
+    for (let i = 0; i < 20; i++) {
+      let possibilities = ngrams[currentGram];
+      if (!possibilities) {
+        break;
+      }
+      let next = random(possibilities);
+      result += next;
+      let len = result.length;
+      currentGram = result.substring(len - order, len);
+    }
+  
+    createP(result);
+  }
