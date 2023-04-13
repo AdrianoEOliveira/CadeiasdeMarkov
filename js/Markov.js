71c55d9e325c77d9e1025ce11ba4ce3e64761cd1
Adriano <adrianoeo@outlook.com>
2023-04-13 20:21:58 -0300

    Corrige formatação

--- js/Markov.js
+++ js/Markov.js
@@ -1,66 +1,44 @@
-export default class Markov 
-{
-  
-  constructor()
-  {
-      this.tiles = []
-      this.possibilidades = []
-      this.mapa = []
- 
+export default class Markov {
+  constructor() {
+    this.tiles = [];
+    this.possibilidades = [];
+    this.mapa = [];
   }
-  RandomIndex(arr) 
-  {
-  let randomIndex = Math.floor(Math.random() * arr.length);
-  if(arr[randomIndex]>0)
-    {
-    return randomIndex;
-    }
-    else
-    {
-      return this.RandomIndex(arr)
+  RandomIndex(arr) {
+    let randomIndex = Math.floor(Math.random() * arr.length);
+    if (arr[randomIndex] > 0) {
+      return randomIndex;
+    } else {
+      return this.RandomIndex(arr);
     }
   }
-    GenerateRandomMap(primeiroTile)
-    {
-      let anterior =primeiroTile
-      for (let l = 0; l < 15; l++) 
-      {
-        this.mapa [l] = []
-          for (let c = 0; c < 15; c++) 
-          {
-          let aux = []
-          aux = aux.concat(this.possibilidades[anterior])
+  GenerateRandomMap(primeiroTile) {
+    let anterior = primeiroTile;
+    for (let l = 0; l < 15; l++) {
+      this.mapa[l] = [];
+      for (let c = 0; c < 15; c++) {
+        if (l == 0 && c == 0) {
+          this.mapa[l][c] = this.tiles[anterior];
+        } else {
+          let aux = [];
+          aux = aux.concat(this.possibilidades[anterior]);
           anterior = this.RandomIndex(aux);
           this.mapa[l][c] = this.tiles[anterior];
-          }
-      
+        }
       }
-      return this.mapa
     }
+    return this.mapa;
+  }
 
   // Add a single state or states
   addStates(tile) {
-
-    this.tiles.push(tile)
-    }
-  setPosssibilidades(possibilite)
-  {
-    this.possibilidades = possibilite
+    this.tiles.push(tile);
+  }
+  setPosssibilidades(possibilite) {
+    this.possibilidades = possibilite;
   }
 
-  iniciar()
-  {
-    return this.GenerateRandomMap
+  iniciar() {
+    return this.GenerateRandomMap;
   }
 }
-
-
-
-
-
-
-
-
-
-
-
