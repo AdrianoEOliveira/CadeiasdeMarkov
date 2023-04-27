4ea56dc6305d112a95b957cdfbfe091dee3891f0
Adriano <adrianoeo@outlook.com>
2023-04-27 20:27:15 -0300

    Tem que diminuir tamanho imagem teste

--- js/Mapa.js
+++ js/Mapa.js
@@ -1,81 +1,74 @@
 import Markov from "./Markov.js";
 
-export default class Mapa
-{
-    constructor(linhas=30,colunas = 30 ,tamanho=32,markov)
-    {
-        this.LINHAS = linhas;
-        this.COLUNAS= colunas;
-        this.TAMANHO =  tamanho;
-        this.tiles = [];
-        for (let l = 0; l < this.LINHAS; l++) 
-        {
-            this.tiles[l] = []
-            for (let c = 0; c < this.COLUNAS; c++) 
-            {
-            this.tiles[l][c] = 0;
-            }
-        this.cena = null;
+export default class Mapa {
+  constructor(linhas = 30, colunas = 30, tamanho = 32, markov) {
+    this.LINHAS = linhas;
+    this.COLUNAS = colunas;
+    this.TAMANHO = tamanho;
+    this.tiles = [];
+    for (let l = 0; l < this.LINHAS; l++) {
+      this.tiles[l] = [];
+      for (let c = 0; c < this.COLUNAS; c++) {
+        this.tiles[l][c] = 0;
+      }
+      this.cena = null;
+    }
+    this.markov = markov;
+  }
+  desenhar(ctx) {
+    let img = new Image();
+    img = this.cena.assets.Img("terreno");
+    let chest = new Image();
+    chest = this.cena.assets.Img("chest");
+    let coin = new Image();
+    coin = this.cena.assets.Img("coin");
+    let linha = 21;
+    let coluna = 8;
+    for (let l = 0; l < this.LINHAS; l++) {
+      for (let c = 0; c < this.COLUNAS; c++) {
+        ctx.drawImage(
+          img,
+          coluna * 32,
+          linha * 32,
+          32,
+          32,
+          c * 32,
+          l * 32,
+          32,
+          32
+        );
+        if (this.tiles[l][c] == 1) {
+          ctx.drawImage(img, 21 * 32, 21 * 32, 32, 32, c * 32, l * 32, 32, 32);
         }
-        this.markov =markov
-    }
-    desenhar(ctx)
-    {
-        let img =new Image();
-        img = this.cena.assets.Img("terreno");
-        let chest = new Image();
-        chest = this.cena.assets.Img("chest")
-        let coin = new Image();
-        coin = this.cena.assets.Img("coin")
-        let linha = 21;
-        let coluna = 8;
-        for (let l = 0; l < this.LINHAS; l++) 
-        {
-            for (let c = 0; c < this.COLUNAS; c++) 
-            {
-
-                ctx.drawImage(img,coluna * 32,linha *32,32,32,
-                    c*32,l*32,32,32);
-                    if(this.tiles[l][c]==1)
-                    {
-                        ctx.drawImage(img,21 * 32,21 *32,32,32,
-                            c*32,l*32,32,32);
-                    }
-                    if(this.tiles[l][c]==2)
-                    {
-                        ctx.drawImage(chest,0,0,32,32,
-                            c*32,l*32,32,32);
-                    }
-                    if(this.tiles[l][c]==3)
-                    {
-                        ctx.drawImage(img, 23* 32 ,23 *32 ,32,32,
-                            c*32,l*32,32,32);
-                    }
-                    if(this.tiles[l][c]==4)
-                    {
-                        ctx.drawImage(img,22 * 32, 21* 32, 32, 32,
-                            c*32, l *32 ,32 ,32);
-                    }
-                    if(this.tiles[l][c]==5)
-                    {
-                        ctx.drawImage(coin,0, 0, 32, 32,
-                            c*32, l *32 ,32 ,32);
-                    }
-
-                ctx.strokeRect(c*this.TAMANHO,l*this.TAMANHO,this.TAMANHO,this.TAMANHO);
-            }
+        if (this.tiles[l][c] == 2) {
+          ctx.drawImage(chest, 0, 0, 32, 32, c * 32, l * 32, 32, 32);
         }
+        if (this.tiles[l][c] == 3) {
+          ctx.drawImage(img, 23 * 32, 23 * 32, 32, 32, c * 32, l * 32, 32, 32);
+        }
+        if (this.tiles[l][c] == 4) {
+          ctx.drawImage(img, 22 * 32, 21 * 32, 32, 32, c * 32, l * 32, 32, 32);
+        }
+        if (this.tiles[l][c] == 5) {
+          ctx.drawImage(coin, 0, 0, 32, 32, c * 32, l * 32, 32, 32);
+        }
+
+        ctx.strokeRect(
+          c * this.TAMANHO,
+          l * this.TAMANHO,
+          this.TAMANHO,
+          this.TAMANHO
+        );
+      }
     }
-    carregaMapa()
-    {
+  }
+  carregaMapa() {
+    this.markov.addStates(1);
+    this.markov.addStates(0);
 
-        this.markov.addStates(1)
-        this.markov.addStates(0)
+    this.tiles = this.markov.iniciar();
+    this.markov.limpa();
 
-        this.tiles = this.markov.iniciar()
-        this.markov.limpa()
-
-        //this.desenhar()
-
-    }
+    //this.desenhar()
+  }
 }
--- js/Markov.js
+++ js/Markov.js
@@ -1,5 +1,5 @@
 export default class Markov {
-  constructor(assets,canvas) {
+  constructor(assets, canvas) {
     this.tiles = [];
     this.possibilidades = [];
     this.mapa = [];
@@ -42,87 +42,73 @@ export default class Markov {
     this.possibilidades = possibilite;
   }
 
-  treino()
-  {
-    let pos = []
-    for (let i = 0; i<this.tiles.length;i++)
-    {
-      pos[i] = []
+  treino() {
+    for (let i = 0; i < this.tiles.length; i++) {
+      this.possibilidades[i] = [];
     }
-    for (let i = 0; i<this.tiles.length;i++)
-    {
-      for (let j = 0; j<this.tiles.length;j++)
-      {
-        pos[i][j] = 0
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = 0; j < this.tiles.length; j++) {
+        this.possibilidades[i][j] = 0;
       }
     }
-    let img =new Image();
+    let img = new Image();
     img = this.assets.Img("Treino");
     this.ctx.drawImage(img, 0, 0);
     //img.style.display = "none";
 
     //let imgData = this.ctx.getImageData(0, 0, this.canvas.width, this.canvas.height);
 
-      // invert colors
-      //for (let i = 16 *4  i < imgData.data.length; i = (4*32)) {
-        //console.log(imgData.data[i],imgData.data[i+1],imgData.data[i+2],imgData.data[3]) 
-        //const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
-        //if(imgData.data[0]== 0 && imgData.data[1] == 0 && imgaData.data[2] == 0)
-        //{
-          //pos[ant][0] = pos[ant][0] +1
-        //}
-        //else
-        //{
-          //pos[ant][1] = pos[ant][1] + 1
-        //}
-        //cont = cont + 1
-      let ant = 0
-      let cont = 0
-      // i = 16 pixel medio do quadrado de 32
-      for(let i = 16 ; i<1920/2 ; i = i + 32)
-      {
-        for(let j = 16; j<1920/2; j = j + 32 )
-        {
-          let pixel = this.ctx.getImageData(j, i, 1, 1);
-          let data = pixel.data;
+    //for (let i = 16 *4  i < imgData.data.length; i = (4*32)) {
+    //console.log(imgData.data[i],imgData.data[i+1],imgData.data[i+2],imgData.data[3])
+    //const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
+    //if(imgData.data[0]== 0 && imgData.data[1] == 0 && imgaData.data[2] == 0)
+    //{
+    //pos[ant][0] = pos[ant][0] +1
+    //}
+    //else
+    //{
+    //pos[ant][1] = pos[ant][1] + 1
+    //}
+    //cont = cont + 1
+    let ant = 0;
+    let cont = 0;
+    // i = 16 pixel medio do quadrado de 32
+    for (let gi = 0; gi<5;gi++){
+      for (let gj = 0; gj<5;gj++){
+    for (let i = gi*5; i < gi*5 + 5; i++) {
+      for (let j = gj*5; j < gj*5+ 5; j++) {
+        let pixel = this.ctx.getImageData(i, j, 1, 1);
+        let data = pixel.data;
         //const rgba = `rgba(${data[0]}, ${data[1]}, ${data[2]}, ${data[3] / 255})`;
         //console.log(data)
-          if(data[0] == 0 && data[1] == 0 && data[2] == 0)
-          {
-            pos[ant][0] = pos[ant][0] + 1
-            ant = 0
-          }
-          else
-          {
-            pos[ant][1] = pos[ant][1] + 1
-            ant = 1
-          }
-          cont = cont + 1
+        if (data[0] == 0 && data[1] == 0 && data[2] == 0) {
+          this.possibilidades[ant][0] = this.possibilidades[ant][0] + 1;
+          ant = 0;
+        } else {
+          this.possibilidades[ant][1] = this.possibilidades[ant][1] + 1;
+          ant = 1;
+        }
+        cont = cont + 1;
         //console.log(rgba)
-          
-        }
       }
-      for ( let i = 0 ; i< this.tiles.length;i++)
-      {
-        for ( let j = 0 ; j< this.tiles.length;j++)
-        {
-          pos[i][j] = pos[i][j]/cont
-        }
-
-      }
-      console.log(pos)
-      this.setPosssibilidades(pos)
     }
+  }
+}
+    for (let i = 0; i < this.tiles.length; i++) {
+      for (let j = 0; j < this.tiles.length; j++) {
+        this.possibilidades[i][j] = this.possibilidades[i][j] / cont;
+      }
+    }
+    console.log(this.possibilidades);
+  }
 
-  iniciar() 
-  {
-    this.treino()
+  iniciar() {
+    this.treino();
     return this.GenerateRandomMap(0);
   }
 
-  limpa()
-  {
-    this.possibilidades = []
-    this.tiles = []
+  limpa() {
+    this.possibilidades = [];
+    this.tiles = [];
   }
 }
