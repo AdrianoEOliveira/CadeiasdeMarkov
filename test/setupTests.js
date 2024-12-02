e166c7f7f383f73a4c5f60735aaeb711333b91ba
Adriano <Adrianoeo@outlook.com>
2024-12-01 22:19:20 -0300

    reinicia configuração

---
+++ test/setupTests.js
@@ -0,0 +1,2 @@
+// Importa as asserções extras do jest-dom
+import '@testing-library/jest-dom';
--- vitest.config.js
+++ vitest.config.js
@@ -1,14 +1,20 @@
 import { defineConfig } from 'vitest/config'
 
+
 export default defineConfig({
   test: {
-    browser: {
-      enabled: true,
-      name: 'chromium',
-      provider: 'playwright',
-      // https://playwright.dev
-      include: ['test/**/*.test.js'],  // Caminho para os testes
-      providerOptions: {},
-    },
+    environment: 'jsdom',
+    setupFiles: './tests/setupTests.js',
+    include: ['test/**/*.test.js'],  // Caminho para os testes
   },
+
+    //  browser: {
+    //  enabled: false,
+    //  name: 'chromium',
+     // provider: 'playwright',
+      // https://playwright.dev
+     // include: ['test/**/*.test.js'],  // Caminho para os testes
+     // providerOptions: {},
+   // },
+  //},
 })
